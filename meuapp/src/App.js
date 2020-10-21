71b5ff2afb2d055f1b6e2eb75669e9556e929ebf
erickjohnson <erick_johnson@live.com>
2020-10-21 18:28:59 -0300

    objects in jsx

--- meuapp/src/App.js
+++ meuapp/src/App.js
@@ -8,11 +8,20 @@ function App() {
   function showNameLastName(lastName) {
     return 'Erick ' + lastName
   }
+
+  const car = {
+    marca : 'Fiat',
+    year: '2009'
+  }
+
   return (
     <>
       {title}
       <p className={active ? 'ativo' : ''}>{random}</p>
       <p>{showNameLastName('Johnson')}</p>
+      <p>
+        {car.marca} {car.year}
+      </p>
     </>
   );
 }
