6c162996781195e6b793bc9e947931c9eff05acd
erickjohnson <erick_johnson@live.com>
2020-10-21 18:27:22 -0300

    functions in jsx

--- meuapp/src/App.js
+++ meuapp/src/App.js
@@ -3,9 +3,16 @@ import React from 'react';
 function App() {
   const random = Math.random();
   const active = true;
+
+  const title = <h1>'JSX Tests' </h1>
+  function showNameLastName(lastName) {
+    return 'Erick ' + lastName
+  }
   return (
     <>
+      {title}
       <p className={active ? 'ativo' : ''}>{random}</p>
+      <p>{showNameLastName('Johnson')}</p>
     </>
   );
 }
